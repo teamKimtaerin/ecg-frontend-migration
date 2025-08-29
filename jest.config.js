9ffaeaffb964285d69f57fdded1da9dccfce0670
Yerinnnn <shinyerin0216@gmail.com>
2025-08-29 21:37:08 +0900

    Chore: Jest의 설정 파일 수정
    - Jest는 유닛 테스트 파일만 실행하고, Playwright는 별도의 CI 스텝에서 yarn test:e2e 명령어로 실행되어 두 테스트 도구가 서로 충돌 없이 역할을 수행

--- jest.config.js
+++ jest.config.js
@@ -11,8 +11,9 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   testMatch: [
-    '**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '**/?(*.)+(spec|test).{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{test,spec}.{js,jsx,ts,tsx}',
+    '<rootDir>/__tests__/**/*.{js,jsx,ts,tsx}',
   ],
   testPathIgnorePatterns: [
     '<rootDir>/.next/',
@@ -20,6 +21,7 @@ const customJestConfig = {
     '<rootDir>/coverage/',
     '<rootDir>/out/',
     '<rootDir>/build/',
+    '<rootDir>/tests/',
   ],
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
